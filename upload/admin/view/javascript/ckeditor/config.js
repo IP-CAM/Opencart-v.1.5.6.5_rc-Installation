48196c85aa7871601029cffa38faab61e4c1ddf9
condor2 <condor20_05@yahoo.it>
2023-01-04 15:11:24 +0200

    Updated CKEditor to 4.20.1

--- upload/admin/view/javascript/ckeditor/config.js
+++ upload/admin/view/javascript/ckeditor/config.js
@@ -13,21 +13,5 @@ CKEDITOR.editorConfig = function( config ) {
 	config.htmlEncodeOutput = false;
 	config.entities = false;
 	config.extraPlugins = 'codemirror';
-	config.codemirror_theme = 'rubyblue';
-	config.toolbar = 'Custom';
-
-	config.toolbar_Custom = [
-		['Source'],
-		['Maximize'],
-		['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-		['NumberedList','BulletedList','-','Outdent','Indent'],
-		['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
-		['SpecialChar'],
-		'/',
-		['Undo','Redo'],
-		['Font','FontSize'],
-		['TextColor','BGColor'],
-		['Link','Unlink','Anchor'],
-		['Image','Table','HorizontalRule']
-	];
+	config.codemirror_theme = 'monokai';
 };
